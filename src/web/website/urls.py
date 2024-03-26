ab8d4c6a85f6415d51361115fbcb7fc2cd5deda2
Ali <>
2024-03-27 03:46:34 +0500

    Templates rendering about, home

--- src/web/website/urls.py
+++ src/web/website/urls.py
@@ -1,5 +1,7 @@
-
+from .views import home, about
+from django.urls import path
 app_name = "website"
 urlpatterns = [
-
+     path('', home, name='home'),
+     path('about/', about, name='about')
 ]
