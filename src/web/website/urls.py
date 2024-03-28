4e5e3854a92c516f35ee34a4bc797fd69156a76b
Ali <>
2024-03-28 11:43:23 +0500

    Templates rendering events,cafedetail,cafe,attraction,attractionlist,attractiondetail,propertytax,conservancytax,watertax,team,teamdetail,tenders,budget,emergencyservices,detpartments,departmentdetails,events,eventdetails

--- src/web/website/urls.py
+++ src/web/website/urls.py
@@ -1,7 +1,27 @@
-from .views import home, about
+from .views import home, about,budget, feedback,team, teamDetails, act, department,departmentDetails,hotels,hotelsDetails,cafedetail,cafe, tenders, event, eventdetail,watertax, conservancytax,propertytax, attractions, attractionlist,attractiondetail
 from django.urls import path
 app_name = "website"
 urlpatterns = [
      path('', home, name='home'),
-     path('about/', about, name='about')
+     path('about/', about, name='about'),
+     path('budget/', budget, name='budget'),
+     path('feedback/', feedback, name='feedback'),
+     path('team/', team, name='team'),
+     path('teamDetails/', teamDetails, name='teamDetails'),
+     path('act/', act, name='gda_act'),
+     path('department/', department, name='departments'),
+     path('departmentDetails/', departmentDetails, name='department_details'),
+     path('hotels/', hotels, name='hotels'),
+     path('hotelsdetails/', hotelsDetails, name='hotels_details'),
+     path('tenders/', tenders, name='tenders'),
+     path('cafe/', cafe, name='cafe'),
+     path('cafedetail/', cafedetail, name='cafedetail'),
+     path('event/', event, name='event'),
+     path('eventdetail/', eventdetail, name='eventdetail'),
+     path('watertax/', watertax, name='watertax'),
+     path('conservancytax/', conservancytax, name='conservancytax'),
+     path('propertytax/', propertytax, name='propertytax'),
+     path('attractions/', attractions, name='attractions'),
+     path('attractionlist/', attractionlist, name='attractionlist'),
+     path('attractiondetail/', attractiondetail, name='attractiondetail'),
 ]
--- src/web/website/views.py
+++ src/web/website/views.py
@@ -7,3 +7,63 @@ def home(request):
 
 def about(request):
     return render(request, 'website/about.html')
+
+def budget(request):
+    return render(request, 'website/budget.html')
+
+def feedback(request):
+    return render(request, 'website/feedback.html')
+
+def team(request):
+    return render(request, 'website/team.html')
+
+def teamDetails(request):
+    return render(request, 'website/teamDetails.html')
+
+def act(request):
+    return render(request, 'website/gda_act.html')
+
+def department(request):
+    return render(request, 'website/departments.html')
+
+def departmentDetails(request):
+    return render(request, 'website/departments_details.html')
+
+def hotels(request):
+    return render(request, 'website/hotels.html')
+
+def hotelsDetails(request):
+    return render(request, 'website/hotels_details.html')
+
+def tenders(request):
+    return render(request, 'website/tenders.html')
+
+def cafe(request):
+    return render(request, 'website/cafe.html')
+
+def cafedetail(request):
+    return render(request, 'website/cafedetail.html')
+
+def event(request):
+    return render(request, 'website/event.html')
+
+def eventdetail(request):
+    return render(request, 'website/eventdetail.html')
+
+def watertax(request):
+    return render(request, 'website/watertax.html')
+
+def conservancytax(request):
+    return render(request, 'website/conservancytax.html')
+
+def propertytax(request):
+    return render(request, 'website/property_tax.html')
+
+def attractions(request):
+    return render(request, 'website/attraction.html')
+
+def attractionlist(request):
+    return render(request, 'website/attractionlist.html')
+
+def attractiondetail(request):
+    return render(request, 'website/attractiondetail.html')
